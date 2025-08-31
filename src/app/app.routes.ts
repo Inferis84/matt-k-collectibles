5ae6fd58ffc5d8523330dc3610683bda7cddcd37
Matthew Klassen <matthewklassen84@gmail.com>
2025-08-31 12:25:15 -0500

    Put together the main part of the home page, hooked up navigation, and added some images for starting with.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Commissions } from './commissions/commissions';
+import { Gallery } from './gallery/gallery';
+import { Store } from './store/store';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home, title: 'Matt K Collectibles' },
+  { path: 'commissions', component: Commissions, title: 'Commissions' },
+  { path: 'gallery', component: Gallery, title: 'Gallery' },
+  { path: 'store', component: Store, title: 'Store' },
+];
